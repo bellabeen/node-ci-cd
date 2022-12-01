8539cb988d6e289637c33b00c8e12f96b089362d
bellabeen <rootsquid@gmail.com>
2022-12-01 11:42:40 +0700

    change text MCP

--- example-express/index.js
+++ example-express/index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const app = express();
 
 app.get("*", (req, res) => {
-  res.json({ message: "welcome to MCP" });
+  res.json({ message: "welcome to MCP NEW CI" });
 });
 
 app.listen(process.env.PORT);
